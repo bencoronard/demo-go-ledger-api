578dd94117b4e79891a30c1c764604e81ff7cb4d
ben@orion <128577990+bencoronard@users.noreply.github.com>
2025-12-30 15:28:54 +0700

    Add resource repository placeholders

--- internal/resource/repository.go
+++ internal/resource/repository.go
@@ -22,22 +22,18 @@ func NewResourceRepo(db *gorm.DB) resourceRepo {
 	return &resourceRepoImpl{db: db}
 }
 
-// delete implements [resourceRepo].
-func (r *resourceRepoImpl) delete(ctx context.Context, ent *resource) error {
-	panic("unimplemented")
-}
-
-// findAll implements [resourceRepo].
 func (r *resourceRepoImpl) findAll(ctx context.Context, page dto.Pageable, createdBy int64) (dto.Slice[resource], error) {
-	panic("unimplemented")
+	return dto.Slice[resource]{}, nil
 }
 
-// findById implements [resourceRepo].
 func (r *resourceRepoImpl) findById(ctx context.Context, id int64, createdBy int64) (*resource, error) {
-	panic("unimplemented")
+	return &resource{}, nil
 }
 
-// save implements [resourceRepo].
 func (r *resourceRepoImpl) save(ctx context.Context, ent *resource) (*resource, error) {
-	panic("unimplemented")
+	return &resource{}, nil
+}
+
+func (r *resourceRepoImpl) delete(ctx context.Context, ent *resource) error {
+	return nil
 }
